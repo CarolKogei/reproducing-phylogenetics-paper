f486dffc1d3903f733e83a7c219524b7a5c26813
JB Mugi <87914905+Mugi-JB@users.noreply.github.com>
2021-11-19 16:47:50 +0300

    Update split_align_merge_stats.sh

--- scripts/split_align_merge_stats.sh
+++ scripts/split_align_merge_stats.sh
@@ -7,6 +7,7 @@ module load bcftools/1.9
 mkdir splits
 mkdir markdups
 mkdir plots
+mkdir stats
 
 for i in $1*
 do
@@ -26,10 +27,10 @@ do
 	samtools merge ${sample%.fastq}_merged.bam markdups/${sample%.fastq}_marked/*
 	samtools stats -c 1,1000,1 ${sample%.fastq}_merged.bam > ${sample%.fastq}_merged_stats
 	#samtools coverage ${sample%.fastq}_merged.bam > ${sample%.fastq}_merged_coverage
-	bcftools mpileup -d 1000 -SgB -Ou --threads 32  -f consensus.fasta ${sample%.fastq}_merged.bam | bcftools call --threads 32 -vmO z -o ${sample%.fastq}.vcf.gz
-	tabix -p vcf ${sample%.fastq}.vcf.gz
-	bcftools stats --threads 32 -F consensus.fasta -s - ${sample%.fastq}.vcf.gz > ${sample%.fastq}.vcf.gz.stats
-	plot-vcfstats -p plots/study.vcf.gz.stats
+	bcftools mpileup -d 1000 -SgB -Ou --threads 32  -f consensus.fasta ${sample%.fastq}_merged.bam | bcftools call --threads 32 -vmO z -o stats/${sample%.fastq}.vcf.gz
+	tabix -p vcf stats/${sample%.fastq}.vcf.gz
+	bcftools stats --threads 32 -F consensus.fasta -s - stats/${sample%.fastq}.vcf.gz > stats/${sample%.fastq}.vcf.gz.stats
+	#plot-vcfstats -p plots/study.vcf.gz.stats
 done
 
 #bcftools filter --threads 32 -O z -o study_filtered.vcf.gz -s LOWQUAL -i'%QUAL>10' study.vcf.gz

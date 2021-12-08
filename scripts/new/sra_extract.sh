c9d9f97a75199c3970baaf80c0f86e7bd66c381d
JB Mugi <87914905+Mugi-JB@users.noreply.github.com>
2021-12-08 17:40:09 +0300

    updated comments

--- scripts/new/sra_extract.sh
+++ scripts/new/sra_extract.sh
@@ -6,13 +6,15 @@ module load sratoolkit/2.11.3
 #This argument is the file containing a list of accession numbers with a new line as a delimitor
 accession_list="$1"
 
-mkdir sra_files
-mkdir fastq_files
-mkdir fastqc_files
+#creates three folders within which relevant documents will be contained
+mkdir sra_files fastq_files fastqc_files
 
+#prefetch from sra to save on computation resources
 prefetch -f yes -O sra_files --option-file $accession_list
 
+#extracts the fastq files from the sra prefetch files and split spots into reads using 32 threads
 fasterq-dump -s --skip-technical -e 32 -O fastq_files sra_files/*/*.sra
 
+#run quality checks with fastqc
 fastqc -o fastqc_files fastq_files/*
 
